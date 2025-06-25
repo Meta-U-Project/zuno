69f03722b0c2b5c021aa4889d98dff4bb7f59ccb
King Igbozuruike <thetechking@meta.com>
2025-06-25 15:53:36 -0700

    completed navbar and part of hero section

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="text">
+            <p className="title">meet your new study sidekick.</p>
+            <p className="subtitle">we'll handle the organizing, focus on the studying.</p>
+        </div>
+        <div className="cta">
+            <button>get started</button>
+        </div>
+    </div>
+  );
+};
+
+
+export default Hero;
